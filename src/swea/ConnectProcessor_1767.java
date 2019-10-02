1bb756dc677e94f23d84f6d93f5a723b26c6bb12
박다예 <sju02030@gmail.com>
2019-10-03 01:43:12 +0900

    swea 시뮬레이션 문제 16진수 비교

--- src/swea/ConnectProcessor_1767.java
+++ src/swea/ConnectProcessor_1767.java
@@ -7,17 +7,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+// 재귀로 풀면 했던 거를 되돌려 놓는 작업이 필요하다
+// 재귀 호출을 여러개 하려면 반드시 return을 신경써야한다
+
 public class ConnectProcessor_1767 {
 	private static int ax[] = { -1, 1, 0, 0 };
 	private static int ay[] = { 0, 0, -1, 1 };
 	private static int[][] map;
-	private static int N;
-	private static int min = Integer.MAX_VALUE;
-	private static int max = 0;
-	private static int count = 0;
-	private static int coreCount = 0;
-	private static boolean isConnectable = false;
 	private static List<Point> core;
+	private static int N;
+	private static int min;
+	private static int max;
+	private static int count;
+	private static int coreCount;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,6 +29,10 @@ public class ConnectProcessor_1767 {
 			N = Integer.parseInt(br.readLine());
 			map = new int[N + 1][N + 1];
 			core = new ArrayList<Point>();
+			min = Integer.MAX_VALUE;
+			max = 0;
+			count = 0;
+			coreCount = 0;
 
 			// 입력
 			for (int i = 1; i <= N; i++) {
@@ -40,7 +46,6 @@ public class ConnectProcessor_1767 {
 			}
 
 			dfs(0);
-
 			System.out.println("#" + (t + 1) + " " + min);
 
 		}
@@ -49,8 +54,10 @@ public class ConnectProcessor_1767 {
 
 	public static void dfs(int now) {
 		if (now == core.size()) {
-			if (max <= coreCount) {
+			if (max < coreCount) {
 				max = coreCount;
+				min = count;
+			} else if(max == coreCount) {
 				min = Math.min(min, count);
 			}
 			return;
@@ -63,6 +70,8 @@ public class ConnectProcessor_1767 {
 		if (x == 1 || x == N || y == 1 || y == N) { // 모서리에 있을 경우
 			coreCount++;
 			dfs(now + 1);
+			coreCount--;
+			return;
 		}
 
 		if (map[x][y] == 1) {
@@ -77,7 +86,7 @@ public class ConnectProcessor_1767 {
 			int nextX = x + ax[i];
 			int nextY = y + ay[i];
 
-			isConnectable = true;
+			boolean isConnectable = true;
 			int tempCount = 0;
 
 			while (nextX > 0 && nextX <= N && nextY > 0 && nextY <= N) {
@@ -105,12 +114,11 @@ public class ConnectProcessor_1767 {
 			if (isConnectable) {
 				coreCount++;
 				count += tempCount;
-				
-			} else if (!isConnectable && i != 0) {
-				coreCount--;
 			}
+			
 			dfs(now + 1); // 현재위치 옆에 있는 core 위치
-
+			// 그냥 coreCount를 dfs함수 인자로 넘겨서 풀었으면 깔끔할 것을,,, idiot~~
+			
 			if (isConnectable) {
 				int preX = nextX - ax[i];
 				int preY = nextY - ay[i];
@@ -119,6 +127,7 @@ public class ConnectProcessor_1767 {
 					preX -= ax[i];
 					preY -= ay[i];
 				}
+				coreCount--;
 				count -= tempCount;
 			}
 		}
---
+++ src/swea/TreasureBoxPassword_5658.java
@@ -0,0 +1,54 @@
+package swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class TreasureBoxPassword_5658 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 0 ; t < T; t++) {
+			List<Long> passwordSet = new ArrayList<Long>();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
+			
+			String password = br.readLine();
+			int tokenLength = N / 4;
+			
+			for(int i = 0; i < tokenLength; i++) {
+				for(int j = 0; j < N; j += tokenLength) { //4가지 면으로 자름
+					String token = password.substring(j, j + tokenLength);
+					
+					long sum = 0;
+					for(int k = 0; k < tokenLength; k++) { //16진수 계산
+						String now = token.charAt(k) + "";
+						int n = Integer.valueOf(now, 16); //16진수에서 10진수로 변환
+						int dec = (int) (n * Math.pow(16, tokenLength - k - 1));
+						sum += dec;
+						
+					}
+					if(!passwordSet.contains(sum))
+						passwordSet.add(sum);					
+					
+				}
+				
+				char end = password.charAt(N-1);
+				password = end + password.substring(0, N-1);
+			}
+			
+			Collections.sort(passwordSet, Comparator.reverseOrder());
+			System.out.println("#" + (t + 1) + " " + passwordSet.get(K - 1));
+		}
+	}
+
+}
