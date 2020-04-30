17e57128cdebf43ae95c06c3fbe0092ffff52e66
박다예 <sju02030@gmail.com>
2020-04-30 23:57:18 +0900

    세잔의 포도주를 연속해서 마시지 않는 최대값_세가지 경우를 비교해 최대값을 구해나가도록 풀이

--- src/dp/WineTasting_2156.java
+++ src/dp/WineTasting_2156.java
@@ -8,26 +8,30 @@ public class WineTasting_2156 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		int n = Integer.parseInt(br.readLine());
-		int N[] = new int[n+2];
-		int dp[][] = new int[n+2][2];
-		
-		for(int i = 1; i <= n; i++) {
+		int N[] = new int[n + 1];
+		int dp[] = new int[n + 1];
+
+		for (int i = 1; i <= n; i++) {
 			N[i] = Integer.parseInt(br.readLine());
 		}
+
+		dp[1] = N[1];
 		
-		dp[1][0] = N[1];
-		dp[1][1] = N[1];
-		dp[2][0] = N[2];
-		dp[2][1] = N[1] + N[2];
-		for(int i = 3; i <= n + 1; i++) {
-			dp[i][0] = Math.max(dp[i-2][0], dp[i-2][1]) + N[i];
-			dp[i][1] = dp[i-1][0] + N[i];
+		if(n == 1) {
+			System.out.println(dp[1]);
+			return;
 		}
+			
 		
-		System.out.println(Math.max(dp[n +1][0], dp[n+1][1]));
-		
+		dp[2] = N[1] + N[2];
+		for (int i = 3; i <= n; i++) {
+			dp[i] = Math.max(Math.max(dp[i-1], dp[i-2] + N[i]), dp[i-3] + N[i] + N[i-1]);
+		}
+
+		System.out.println(dp[n]);
+
 	}
 
 }
